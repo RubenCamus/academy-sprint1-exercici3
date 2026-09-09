7c19ba497f803a47efc995da39f43911f99a45f4
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-09 10:41:53 +0200

    feat: Nivell 1 Exercici 3 done

--- nivell1.php
+++ nivell1.php
@@ -9,13 +9,31 @@ declare(strict_types = 1 );
 // }
 //
 
-$secArray = ["pep", 300, true, "abc", "str", 99];
+// $secArray = ["pep", 300, true, "abc", "str", 99];
 
-echo count($secArray);
-echo PHP_EOL;
-array_splice($secArray, 1, 1);
-var_dump($secArray);
-echo PHP_EOL;
-echo count($secArray);
-var_dump($secArray);
+// echo count($secArray);
+// echo PHP_EOL;
+// array_splice($secArray, 1, 1);
+// var_dump($secArray);
+// echo PHP_EOL;
+// echo count($secArray);
+// var_dump($secArray);
+//
+
+function checkCharacter(array $arr, string $char) {
+    $counter = 0;
+    for($i = 0; $i < count($arr); $i++) {
+        if(str_contains($arr[$i], $char)) {
+            $counter++;
+            if ($counter == count($arr)) {
+                echo "Character in every word";
+                return true;
+            }
+        } else {
+            echo "Character not in every word";
+            return false;
+        }
+    }
+}
+checkCharacter(["holaa", "patata", "bonito"], "a");
 ?>
