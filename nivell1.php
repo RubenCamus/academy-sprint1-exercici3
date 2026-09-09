6e083a97f0378d66f4d406c33c9c02d4c5d2b362
RubenCamus <Ruben-4-millet@hotmail.com>
2026-09-09 10:26:47 +0200

    feat: Nivell 1 Exercici 2 done.

--- nivell1.php
+++ nivell1.php
@@ -1,10 +1,21 @@
 <?php
 declare(strict_types = 1 );
 
-$arr = [10, 20, 0, 1000, 999];
+// $arr = [10, 20, 0, 1000, 999];
 
-for($i = 0; $i < count($arr); $i++) {
-    echo "Item $i is: $arr[$i]";
-    echo PHP_EOL;
-}
+// for($i = 0; $i < count($arr); $i++) {
+//     echo "Item $i is: $arr[$i]";
+//     echo PHP_EOL;
+// }
+//
+
+$secArray = ["pep", 300, true, "abc", "str", 99];
+
+echo count($secArray);
+echo PHP_EOL;
+array_splice($secArray, 1, 1);
+var_dump($secArray);
+echo PHP_EOL;
+echo count($secArray);
+var_dump($secArray);
 ?>
